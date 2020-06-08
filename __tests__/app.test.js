8802455ec42af7c999a97691f16ca070b4f03052
Jpendy <Jpendy256@gmail.com>
2020-06-08 13:47:42 -0700

    got / route and /red route tested

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,23 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('createResponse', () => {
+    
+  it('handles the / route', () => {
+
+    return request(app)
+      .get('/')
+      .then(res => {
+        expect(res.text).toEqual('hi');
+      });
+  });
+
+  it('handles the /red route', () => {
+
+    return request(app)
+      .get('/red')
+      .then(res => {
+        expect(res.text).toEqual('<h1>red</h1>');
+      });
+  });
 
 });
